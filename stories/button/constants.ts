f82a26276bfe5550c01f5c3e40d0fb53ffc07971
makoto <oic.makotoharris@gmail.com>
2026-01-25 19:57:24 -0800

    add: style - AppButton

--- stories/button/constants.ts
+++ stories/button/constants.ts
@@ -3,4 +3,5 @@ export const BUTTON_VARIANT = {
   neutral: "NEUTRAL", // used for close
   new: "NEW", // used for new page
   danger: "DANGER", // used for delete
+  outline: "OUTLINE",
 } as const;
